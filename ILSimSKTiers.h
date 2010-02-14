c9a41552a7470af8095417a5e89ab0aa056a81a8
Emanuele Vulcano (Infinite Labs) <me@infinite-labs.net>
2010-02-14 11:04:51 +0100

    EUR and GBP storefronts now support all tiers. Added JPY storefront.

--- ILSimSKTiers.h
+++ ILSimSKTiers.h
@@ -8,10 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+// The string values of these constants are the currency codes just after the _ (for example, @"USD" for kILSimStorefront_USD).
 extern NSString* const kILSimStorefront_USD;
 extern NSString* const kILSimStorefront_EUR;
 extern NSString* const kILSimStorefront_CAD;
 extern NSString* const kILSimStorefront_GBP;
+extern NSString* const kILSimStorefront_JPY;
 
 
 extern NSString* ILSimSKCurrentStorefront();
